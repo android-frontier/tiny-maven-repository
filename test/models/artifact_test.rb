5e2ac8de6d884a38b90493dc78491dbb692e9e32
FUJI Goro (gfx) <gfuji@cpan.org>
2014-07-08 09:17:33 +0900

    more tests

--- test/models/artifact_test.rb
+++ test/models/artifact_test.rb
@@ -5,6 +5,9 @@ class ArtifactTest < ActiveSupport::TestCase
     metadata = File.read('test/fixtures/maven-metadata.xml')
     artifact = Artifact.new(metadata)
     assert do
+      artifact.group_id == 'com.github.gfx.util'
+      artifact.artifact_id == 'weak-identity-hash-map'
+      artifact.version == '2.0.1'
       artifact.as_dependency == 'com.github.gfx.util:weak-identity-hash-map:2.0.1'
     end
   end
