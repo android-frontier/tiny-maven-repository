0f5ec863170a9845cb3e6f075366e73fec48935e
Shota Fukumori (sora_h) <sorah@cookpad.com>
2014-07-09 17:55:54 +0900

    remove old configuration; use secrets.yml

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,10 +79,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-
-  unless ENV["ARTIFACT_ROOT_PATH"].present?
-    raise "No ARTIFACT_ROOT_PATH present"
-  end
-  config.artifact_root_path = Pathname.new(ENV["ARTIFACT_ROOT_PATH"])
 end
