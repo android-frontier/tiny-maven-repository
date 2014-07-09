fc5cb59458b845adcf5db7135107f6199890e5dd
FUJI Goro (gfx) <gfuji@cpan.org>
2014-07-09 22:37:44 +0900

    make site name  configurable

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def site_name
+    Rails.application.config.site[:name]
+  end
+
+  def site_url
+    Rails.application.config.site[:url]
+  end
+
+  def site_maven_repository_url
+    Rails.application.config.site[:maven_repository_url]
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,12 @@ module TinyMavenRepository
   class Application < Rails::Application
     config.autoload_paths << File.expand_path("../lib", __FILE__)
 
+    site_config_file = Rails.root.join('config/site.yml')
+    unless site_config_file.exist?
+      site_config_file = Rails.root.join('config/site.yml.sample')
+    end
+    Rails.application.config.site = YAML.load_file(site_config_file).with_indifferent_access
+
     if Rails.application.secrets.x_sendfile_header
       config.action_dispatch.x_sendfile_header = Rails.application.secrets.x_sendfile_header
     end
