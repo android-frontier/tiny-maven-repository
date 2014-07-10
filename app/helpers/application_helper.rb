e34564c0456345216eb72264e946ce4984ffd93b
Fuji, Goro <goro-fuji@cookpad.com>
2014-07-10 11:34:27 +0900

    use secrets.yml (that's the Rails way)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,13 @@
 module ApplicationHelper
   def site_name
-    Rails.application.config.site[:name]
+    Rails.application.secrets.site['name']
   end
 
   def site_url
-    Rails.application.config.site[:url]
+    Rails.application.secrets.site['url']
   end
 
   def site_maven_repository_url
-    Rails.application.config.site[:maven_repository_url]
+    Rails.application.secrets.site['maven_repository_url']
   end
 end
